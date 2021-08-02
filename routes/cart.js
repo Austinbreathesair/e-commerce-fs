2b04cd17bcc7caa4c0beb2cce7f624daa367779b
Austinbreathesair <austinbreathesair@gmail.com>
2021-08-02 21:19:54 +0200

    feat: added cart and order functionality

---
+++ routes/cart.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const cartController = require('../controller/cartControllers');
+const router = Router();
+router.get('/cart/:id', cartController.get_cart_items);
+router.post('/cart/:id', cartController.add_cart_item);
+router.delete('/cart/:userId/:itemId', cartController.delete_item);
+
+module.exports = router;
